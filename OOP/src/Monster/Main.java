943aa42711ce7033d83c88adccb43e56126e2c43
Daleep Singh <daleepsingh@Daleeps-MacBook-Air.local>
2022-01-13 14:38:51 -0500

    added baseline monster class

---
+++ OOP/src/Monster/Main.java
@@ -0,0 +1,8 @@
+package Monster;
+
+public class Main {
+    public static void main(string[] args){
+        Monster one = new Monster();
+
+    }
+}
--- OOP/src/Monster/Monster.java
+++ OOP/src/Monster/Monster.java
@@ -1,4 +1,24 @@
 package Monster;
 
 public class Monster {
+    private int health = 100;
+    private int attackSpeed = 10;
+    private int attackDamage = 10;
+
+    public void meleeAttack(Monster x){
+        x.health -= 10;
+    }
+
+    public void rangedAttack(Monster x){
+        x.health -= 15;
+    }
+    public int getHealth(){
+        return health;
+    }
+    public int getattackSpeed(){
+        return attackSpeed;
+    }
+    public int getattackDamage(){
+        return attackDamage;
+    }
 }
